7126f04a396378909878ea10ecf43cf96f3c92f9
Jing <1013263@student.sae.edu.au>
2019-10-12 22:25:17 +1100

    simple AI following and patrolling behaviors done

---
+++ Assets/Scripts/AIIdleBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIIdleBehavior : StateMachineBehaviour
+{
+    private Transform playerPos;
+    private Transform AIPos;
+    public float playerDetect;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        AIPos = GameObject.FindGameObjectWithTag("Minion").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(Distance() < playerDetect)
+        animator.SetBool("isFollowing", true);
+    }
+
+    private float Distance()
+    {
+        float d = Vector3.Distance(playerPos.position, AIPos.position);
+        return d;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
