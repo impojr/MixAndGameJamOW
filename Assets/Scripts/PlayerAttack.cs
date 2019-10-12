5d46dcfd9f10212a33ce984ec2e696f2dc5de130
impoimpoimpo <aimpo@outlook.com>
2019-10-12 16:09:36 +1030

    Added player sword slash

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] GameObject sword;
+
+    void Update()
+    {
+        ProcessAttack();
+    }
+
+    private void ProcessAttack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            sword.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public void EndAttack()
+    {
+        gameObject.SetActive(false);
+    }
+}
