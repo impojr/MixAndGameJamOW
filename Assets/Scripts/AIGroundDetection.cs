4d6d672d8e4451bf31c18b8ddaeb7c17a761d31c
Jing <1013263@student.sae.edu.au>
2019-10-12 16:21:35 +1100

    AI ground detection trigger not triggering

---
+++ Assets/Scripts/AIGroundDetection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGroundDetection : MonoBehaviour
+{
+    public Patrol AI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AI.isGrounded = true;
+    }
+}
