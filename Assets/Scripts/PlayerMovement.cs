dbc8595e599b960cd0cb5b0fd5afccf95ac883cd
Jing <1013263@student.sae.edu.au>
2019-10-12 12:58:27 +1100

    Basic player movement set up

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
